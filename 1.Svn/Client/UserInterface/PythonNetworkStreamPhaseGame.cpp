456ffef07507b8f1b5ff971c5d6327397dc2b4c4
Muhammet Ali **** <33325253+blackdragonx61@users.noreply.github.com>
2019-03-16 20:57:54 +0300

    Update PythonNetworkStreamPhaseGame.cpp

--- 1.Svn/Client/UserInterface/PythonNetworkStreamPhaseGame.cpp
+++ 1.Svn/Client/UserInterface/PythonNetworkStreamPhaseGame.cpp
@@ -4,14 +4,12 @@
 ///Add
 #ifdef ENABLE_WHISPER_RENEWAL
 	#include "PythonWhisper.h"
-	if (CPythonChat::WHISPER_TYPE_NOT_EXIST != whisperPacket.bType) {
-		if (std::string(buf).find("|?whisper_renewal>|") != std::string::npos) {
-			CPythonWhisper::Instance().AddName(whisperPacket.szNameFrom, CPythonWhisper::TARGET);
-			return true;
-		}
-		else if (std::string(buf).find("|?whisper_renewal<|") != std::string::npos) {
-			CPythonWhisper::Instance().DeleteName(whisperPacket.szNameFrom, CPythonWhisper::TARGET);
-			return true;
-		}
+	if (std::string(buf).find("|?whisper_renewal>|") != std::string::npos) {
+		CPythonWhisper::Instance().AddName(whisperPacket.szNameFrom, CPythonWhisper::TARGET);
+		return true;
+	}
+	else if (std::string(buf).find("|?whisper_renewal<|") != std::string::npos) {
+		CPythonWhisper::Instance().DeleteName(whisperPacket.szNameFrom, CPythonWhisper::TARGET);
+		return true;
 	}
 #endif	
